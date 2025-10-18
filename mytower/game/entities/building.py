c0357d3ccb24e96d33150e1f4e99bb0ba78345a8
Ryan O. <ryanosterday@mac.com>
2025-10-18 11:06:48 -0400

    Remove person protocol from building

--- mytower/game/entities/building.py
+++ mytower/game/entities/building.py
@@ -1,23 +1,22 @@
 from __future__ import annotations  # Defer type evaluation
 
-from typing import Dict, List, Final, override
+from typing import Dict, Final, List, override
 
 from pygame import Surface
 
-from mytower.game.core.units import Blocks, Time
-from mytower.game.utilities.logger import LoggerProvider, MyTowerLogger
 from mytower.game.core.types import FloorType
+from mytower.game.core.units import Blocks, Time
+from mytower.game.entities.entities_protocol import \
+    ElevatorBankList  # Use alias
+from mytower.game.entities.entities_protocol import ElevatorList  # Use alias
+from mytower.game.entities.entities_protocol import FloorList  # Use alias
+from mytower.game.entities.entities_protocol import (BuildingProtocol,
+                                                     ElevatorBankProtocol,
+                                                     ElevatorProtocol,
+                                                     FloorProtocol)
 from mytower.game.entities.floor import Floor
-from mytower.game.entities.entities_protocol import (
-    BuildingProtocol,
-    ElevatorProtocol,
-    PersonProtocol,
-    ElevatorBankProtocol,
-    FloorProtocol,
-    ElevatorList,  # Use alias
-    ElevatorBankList,  # Use alias
-    FloorList  # Use alias
-)
+from mytower.game.utilities.logger import LoggerProvider, MyTowerLogger
+
 
 class Building(BuildingProtocol):
     """
